2e5adb401ef746a8019559688ca722666602cd2e
DeepSeeker <nlyskv12@gmail.com>
2025-10-07 16:32:46 +0300

    Create Base test and Pet test (demo)

---
+++ Tests/BaseTest.cs
@@ -0,0 +1,38 @@
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using Allure.Commons;
+using Allure.NUnit;
+
+namespace PetstoreTests.Tests
+{
+    [AllureNUnit]
+    public class BaseTest
+    {
+        protected static ExtentReports _extent;
+        protected ExtentTest _test;
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var htmlReporter = new ExtentSparkReporter("Reports/ExtentReport.html");
+            _extent = new ExtentReports();
+            _extent.AttachReporter(htmlReporter);
+        }
+        [SetUp]
+        public void SetUp()
+        {
+            _test = _extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+                _test.Fail(TestContext.CurrentContext.Result.Message);
+            else
+                _test.Pass("Test passed");
+
+            _extent.Flush();
+        }
+    }
+}
---
+++ Tests/PetTests.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using PetstoreTests.Helpers;
+using System.Net;
+using System.Threading.Tasks;
+using Allure.Net.Commons;
+using Allure.NUnit.Attributes;
+
+namespace PetstoreTests.Tests
+{
+    [AllureSuite("Pet Testt")]
+    public class PetTests : BaseTest
+    {
+        private ApiClient _apiClient;
+        [SetUp]
+        public void Init()
+        {
+            _apiClient = new ApiClient();
+        }
+
+        [Test(Description = "Get pet by ID")]
+        [AllureTag("API")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("Nikita")]
+        public async Task GetPetById_ShouldReturnPet()
+        {
+            var response = await _apiClient.GetAsync("pet/1");
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.That(content.Contains(@"""id"":1"), "Response does not contain id=1");
+        }
+
+        [Test(Description = "Delete pet by ID")]
+        [AllureTag("API")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureOwner("Nikita")]
+        public async Task DeletePetByID_ShouldReturnSuccess()
+        {
+            var response = await _apiClient.DeleteAsync("pet/1");
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
